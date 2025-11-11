05fd47e6d145562e75a7c9b0192364792e16ee80
jgarw <joegarwood@proton.me>
2025-11-11 08:22:38 -0500

    added qr code generator

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, UploadFile, Form
 from fastapi.responses import FileResponse
 from converter import convert_md_to_pdf
+from qr_generator import generate_qr_code
 import os, uuid
 
 app = FastAPI()
@@ -19,12 +20,32 @@ async def convert(file: UploadFile):
     input_path = f"uploads/{file_id}.md"
     output_path = f"outputs/{file_id}.pdf"
 
+    # save uploaded file contents into new uuid filename
     with open(input_path, "wb") as f:
         contents = await file.read()
         f.write(contents)
 
+    # convert md to pdf, storing pdf in output_path
     convert_md_to_pdf(input_path, output_path)
 
+    generate_qr_code(file_id)
+
+    # return generated pdf
+    return FileResponse(
+        path=output_path,
+        media_type="application/pdf",
+        filename=f"{file_id}.pdf",
+    )    
+
+# function to download specific file from route
+@app.get("/download/{file_id}")
+async def download(file_id: str):
+
+    output_path = f"outputs/{file_id}.pdf"
+
+    if not os.path.exists(output_path):
+        return {"error": "File not found"}
+    
     # return generated pdf
     return FileResponse(
         path=output_path,
